41429ab6ac3b837315c63d07d9757f4a7e271a82
Janusz Chudzynski <jchudzynski@uwf.edu>
2012-08-20 13:43:53 -0500

    Added Project Files

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DataStructuresAndAlgorithms
+//
+//  Created by Janusz Chudzynski on 8/20/12.
+//  Copyright (c) 2012 Janusz Chudzynski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
